4ea0937628d96973d7a2343c5fcef6347bae6ed6
Kushal-kumarreddy <vtu25141@veltech.edu.in>
2026-02-05 22:51:21 +0530

    Time: 29 ms (84.28%) | Memory: 146.8 MB (61.26%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || k == 0) return new int[0];
+
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+        Deque<Integer> dq = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+
+            // 1. Remove elements out of window
+            if (!dq.isEmpty() && dq.peekFirst() <= i - k) {
+                dq.pollFirst();
+            }
+
+            // 2. Remove smaller elements
+            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) {
+                dq.pollLast();
+            }
+
+            // 3. Add current index
+            dq.offerLast(i);
+
+            // 4. Store result
+            if (i >= k - 1) {
+                result[i - k + 1] = nums[dq.peekFirst()];
+            }
+        }
+        return result;
+    }
+}
